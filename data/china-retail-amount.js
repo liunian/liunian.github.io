bb734eb29c4fce3c35312f9408b8357ef630aa16
liunian <dengjij@gmail.com>
2018-07-04 11:19:42 +0800

    Site updated at 2018-07-04 03:19:42 UTC

---
+++ data/china-retail-amount.js
@@ -0,0 +1,503 @@
+// 社会消费品零售总额 
+// 来源：http://www.gov.cn/shuju/2017sjcs/sjxqpage.htm
+var chinaRetailData = {
+  "header": {
+    "time": "统计时间",
+    "retailAmount": "社会消费品零售总额（亿元）",
+    "monthlyGrowth": "社会消费品零售总额月度同比增长（%）",
+    "largeBizRetailAmount": "限额以上企业消费品零售额（亿元）",
+    "largeBizMonthlyGrowth": "限额以上企业消费品零售额月度同比增长（%）"
+  },
+  "data": [
+    {
+      "time": "2012年1-2月",
+      "retailAmount": "33668.6",
+      "monthlyGrowth": "14.7",
+      "largeBizRetailAmount": "15238.3",
+      "largeBizMonthlyGrowth": "14.9"
+    },
+    {
+      "time": "2012年3月",
+      "retailAmount": "15650.2",
+      "monthlyGrowth": "15.2",
+      "largeBizRetailAmount": "7686.0",
+      "largeBizMonthlyGrowth": "16.9"
+    },
+    {
+      "time": "2012年4月",
+      "retailAmount": "15603.1",
+      "monthlyGrowth": "14.1",
+      "largeBizRetailAmount": "7482.6",
+      "largeBizMonthlyGrowth": "14.6"
+    },
+    {
+      "time": "2012年5月",
+      "retailAmount": "16714.8",
+      "monthlyGrowth": "13.8",
+      "largeBizRetailAmount": "7922.5",
+      "largeBizMonthlyGrowth": "14.0"
+    },
+    {
+      "time": "2012年6月",
+      "retailAmount": "16584.9",
+      "monthlyGrowth": "13.7",
+      "largeBizRetailAmount": "8222.8",
+      "largeBizMonthlyGrowth": "14.0"
+    },
+    {
+      "time": "2012年7月",
+      "retailAmount": "16314.9",
+      "monthlyGrowth": "13.1",
+      "largeBizRetailAmount": "7812.0",
+      "largeBizMonthlyGrowth": "13.0"
+    },
+    {
+      "time": "2012年8月",
+      "retailAmount": "16658.9",
+      "monthlyGrowth": "13.2",
+      "largeBizRetailAmount": "8148.7",
+      "largeBizMonthlyGrowth": "13.1"
+    },
+    {
+      "time": "2012年9月",
+      "retailAmount": "18226.6",
+      "monthlyGrowth": "14.2",
+      "largeBizRetailAmount": "8930.7",
+      "largeBizMonthlyGrowth": "14.7"
+    },
+    {
+      "time": "2012年10月",
+      "retailAmount": "18933.8",
+      "monthlyGrowth": "14.5",
+      "largeBizRetailAmount": "9022.8",
+      "largeBizMonthlyGrowth": "14.8"
+    },
+    {
+      "time": "2012年11月",
+      "retailAmount": "18476.7",
+      "monthlyGrowth": "14.9",
+      "largeBizRetailAmount": "9394.5",
+      "largeBizMonthlyGrowth": "15.1"
+    },
+    {
+      "time": "2012年12月",
+      "retailAmount": "20334.2",
+      "monthlyGrowth": "15.2",
+      "largeBizRetailAmount": "10773.4",
+      "largeBizMonthlyGrowth": "15.3"
+    },
+    {
+      "time": "2013年1-2月",
+      "retailAmount": "37809.8",
+      "monthlyGrowth": "12.3",
+      "largeBizRetailAmount": "18001.4",
+      "largeBizMonthlyGrowth": "10.2"
+    },
+    {
+      "time": "2013年3月",
+      "retailAmount": "17641.2",
+      "monthlyGrowth": "12.6",
+      "largeBizRetailAmount": "9075.7",
+      "largeBizMonthlyGrowth": "10.5"
+    },
+    {
+      "time": "2013年4月",
+      "retailAmount": "17600.3",
+      "monthlyGrowth": "12.8",
+      "largeBizRetailAmount": "8950.3",
+      "largeBizMonthlyGrowth": "11.5"
+    },
+    {
+      "time": "2013年5月",
+      "retailAmount": "18886.3",
+      "monthlyGrowth": "12.9",
+      "largeBizRetailAmount": "9371.1",
+      "largeBizMonthlyGrowth": "10.7"
+    },
+    {
+      "time": "2013年6月",
+      "retailAmount": "18826.7",
+      "monthlyGrowth": "13.3",
+      "largeBizRetailAmount": "9764.4",
+      "largeBizMonthlyGrowth": "11.5"
+    },
+    {
+      "time": "2013年7月",
+      "retailAmount": "18513.2",
+      "monthlyGrowth": "13.2",
+      "largeBizRetailAmount": "9158.9",
+      "largeBizMonthlyGrowth": "11.2"
+    },
+    {
+      "time": "2013年8月",
+      "retailAmount": "18886.2",
+      "monthlyGrowth": "13.4",
+      "largeBizRetailAmount": "9490.3",
+      "largeBizMonthlyGrowth": "11.5"
+    },
+    {
+      "time": "2013年9月",
+      "retailAmount": "20653.3",
+      "monthlyGrowth": "13.3",
+      "largeBizRetailAmount": "10526.0",
+      "largeBizMonthlyGrowth": "12.4"
+    },
+    {
+      "time": "2013年10月",
+      "retailAmount": "21491.3",
+      "monthlyGrowth": "13.3",
+      "largeBizRetailAmount": "10578.9",
+      "largeBizMonthlyGrowth": "12.4"
+    },
+    {
+      "time": "2013年11月",
+      "retailAmount": "21011.9",
+      "monthlyGrowth": "13.7",
+      "largeBizRetailAmount": "11055.0",
+      "largeBizMonthlyGrowth": "13.1"
+    },
+    {
+      "time": "2013年12月",
+      "retailAmount": "23059.7",
+      "monthlyGrowth": "13.6",
+      "largeBizRetailAmount": "12641.4",
+      "largeBizMonthlyGrowth": "13.0"
+    },
+    {
+      "time": "2014年1-2月",
+      "retailAmount": "42280.7",
+      "monthlyGrowth": "11.8",
+      "largeBizRetailAmount": "20132.7",
+      "largeBizMonthlyGrowth": "9.5"
+    },
+    {
+      "time": "2014年3月",
+      "retailAmount": "19800.6",
+      "monthlyGrowth": "12.2",
+      "largeBizRetailAmount": "10230.7",
+      "largeBizMonthlyGrowth": "10.2"
+    },
+    {
+      "time": "2014年4月",
+      "retailAmount": "19701.2",
+      "monthlyGrowth": "11.9",
+      "largeBizRetailAmount": "10009.6",
+      "largeBizMonthlyGrowth": "9.4"
+    },
+    {
+      "time": "2014年5月",
+      "retailAmount": "21249.8",
+      "monthlyGrowth": "12.5",
+      "largeBizRetailAmount": "10559.8",
+      "largeBizMonthlyGrowth": "9.9"
+    },
+    {
+      "time": "2014年6月",
+      "retailAmount": "21166.4",
+      "monthlyGrowth": "12.4",
+      "largeBizRetailAmount": "11102.8",
+      "largeBizMonthlyGrowth": "10.2"
+    },
+    {
+      "time": "2014年7月",
+      "retailAmount": "20775.8",
+      "monthlyGrowth": "12.2",
+      "largeBizRetailAmount": "10359.9",
+      "largeBizMonthlyGrowth": "9.7"
+    },
+    {
+      "time": "2014年8月",
+      "retailAmount": "21133.9",
+      "monthlyGrowth": "11.9",
+      "largeBizRetailAmount": "10466.1",
+      "largeBizMonthlyGrowth": "9.0"
+    },
+    {
+      "time": "2014年9月",
+      "retailAmount": "23042.4",
+      "monthlyGrowth": "11.6",
+      "largeBizRetailAmount": "11581.8",
+      "largeBizMonthlyGrowth": "8.5"
+    },
+    {
+      "time": "2014年10月",
+      "retailAmount": "23967.2",
+      "monthlyGrowth": "11.5",
+      "largeBizRetailAmount": "11744.6",
+      "largeBizMonthlyGrowth": "8.3"
+    },
+    {
+      "time": "2014年11月",
+      "retailAmount": "23474.7",
+      "monthlyGrowth": "11.7",
+      "largeBizRetailAmount": "11793.9",
+      "largeBizMonthlyGrowth": "8.7"
+    },
+    {
+      "time": "2014年12月",
+      "retailAmount": "25801.3",
+      "monthlyGrowth": "11.9",
+      "largeBizRetailAmount": "14274.2",
+      "largeBizMonthlyGrowth": "9.4"
+    },
+    {
+      "time": "2015年1-2月",
+      "retailAmount": "47992.5",
+      "monthlyGrowth": "10.7",
+      "largeBizRetailAmount": "21840.4",
+      "largeBizMonthlyGrowth": "8.1"
+    },
+    {
+      "time": "2015年3月",
+      "retailAmount": "22722.8",
+      "monthlyGrowth": "10.2",
+      "largeBizRetailAmount": "10833.5",
+      "largeBizMonthlyGrowth": "7.1"
+    },
+    {
+      "time": "2015年4月",
+      "retailAmount": "22386.7",
+      "monthlyGrowth": "10.0",
+      "largeBizRetailAmount": "10564.2",
+      "largeBizMonthlyGrowth": "6.7"
+    },
+    {
+      "time": "2015年5月",
+      "retailAmount": "24194.8",
+      "monthlyGrowth": "10.1",
+      "largeBizRetailAmount": "11180.1",
+      "largeBizMonthlyGrowth": "6.8"
+    },
+    {
+      "time": "2015年6月",
+      "retailAmount": "24280.3",
+      "monthlyGrowth": "10.6",
+      "largeBizRetailAmount": "12009.4",
+      "largeBizMonthlyGrowth": "7.8"
+    },
+    {
+      "time": "2015年7月",
+      "retailAmount": "24338.8",
+      "monthlyGrowth": "10.5",
+      "largeBizRetailAmount": "11005.3",
+      "largeBizMonthlyGrowth": "7.3"
+    },
+    {
+      "time": "2015年8月",
+      "retailAmount": "24893.4",
+      "monthlyGrowth": "10.8",
+      "largeBizRetailAmount": "11256.6",
+      "largeBizMonthlyGrowth": "8.0"
+    },
+    {
+      "time": "2015年9月",
+      "retailAmount": "25270.6",
+      "monthlyGrowth": "10.9",
+      "largeBizRetailAmount": "12484.1",
+      "largeBizMonthlyGrowth": "8.2"
+    },
+    {
+      "time": "2015年10月",
+      "retailAmount": "28278.9",
+      "monthlyGrowth": "11.0",
+      "largeBizRetailAmount": "12612.2",
+      "largeBizMonthlyGrowth": "8.4"
+    },
+    {
+      "time": "2015年11月",
+      "retailAmount": "27937.3",
+      "monthlyGrowth": "11.2",
+      "largeBizRetailAmount": "13502.7",
+      "largeBizMonthlyGrowth": "8.8"
+    },
+    {
+      "time": "2015年12月",
+      "retailAmount": "28634.6",
+      "monthlyGrowth": "11.1",
+      "largeBizRetailAmount": "15367.0",
+      "largeBizMonthlyGrowth": "8.6"
+    },
+    {
+      "time": "2016年1-2月",
+      "retailAmount": "52910.3",
+      "monthlyGrowth": "10.2",
+      "largeBizRetailAmount": "23080.0",
+      "largeBizMonthlyGrowth": "7.6"
+    },
+    {
+      "time": "2016年3月",
+      "retailAmount": "25114.1",
+      "monthlyGrowth": "10.5",
+      "largeBizRetailAmount": "11717.1",
+      "largeBizMonthlyGrowth": "8.6"
+    },
+    {
+      "time": "2016年4月",
+      "retailAmount": "24645.8",
+      "monthlyGrowth": "10.1",
+      "largeBizRetailAmount": "11264.3",
+      "largeBizMonthlyGrowth": "6.7"
+    },
+    {
+      "time": "2016年5月",
+      "retailAmount": "26610.7",
+      "monthlyGrowth": "10.0",
+      "largeBizRetailAmount": "11884.9",
+      "largeBizMonthlyGrowth": "6.5"
+    },
+    {
+      "time": "2016年6月",
+      "retailAmount": "26857.4",
+      "monthlyGrowth": "10.6",
+      "largeBizRetailAmount": "13005.8",
+      "largeBizMonthlyGrowth": "8.1"
+    },
+    {
+      "time": "2016年7月",
+      "retailAmount": "26827.4",
+      "monthlyGrowth": "10.2",
+      "largeBizRetailAmount": "11805.7",
+      "largeBizMonthlyGrowth": "7.3"
+    },
+    {
+      "time": "2016年8月",
+      "retailAmount": "27539.6",
+      "monthlyGrowth": "10.6",
+      "largeBizRetailAmount": "12249.7",
+      "largeBizMonthlyGrowth": "8.5"
+    },
+    {
+      "time": "2016年9月",
+      "retailAmount": "27976.4",
+      "monthlyGrowth": "10.7",
+      "largeBizRetailAmount": "13503.9",
+      "largeBizMonthlyGrowth": "8.8"
+    },
+    {
+      "time": "2016年10月",
+      "retailAmount": "31119.2",
+      "monthlyGrowth": "10.0",
+      "largeBizRetailAmount": "13507.7",
+      "largeBizMonthlyGrowth": "7.5"
+    },
+    {
+      "time": "2016年11月",
+      "retailAmount": "30958.5",
+      "monthlyGrowth": "10.8",
+      "largeBizRetailAmount": "14791.8",
+      "largeBizMonthlyGrowth": "9.5"
+    },
+    {
+      "time": "2016年12月",
+      "retailAmount": "31757.0",
+      "monthlyGrowth": "10.9",
+      "largeBizRetailAmount": "16945.0",
+      "largeBizMonthlyGrowth": "9.8"
+    },
+    {
+      "time": "2017年1-2月",
+      "retailAmount": "57959.7",
+      "monthlyGrowth": "9.5",
+      "largeBizRetailAmount": "24679.7",
+      "largeBizMonthlyGrowth": "6.8"
+    },
+    {
+      "time": "2017年3月",
+      "retailAmount": "27863.7",
+      "monthlyGrowth": "10.9",
+      "largeBizRetailAmount": "12873.3",
+      "largeBizMonthlyGrowth": "10.0"
+    },
+    {
+      "time": "2017年4月",
+      "retailAmount": "27278.5",
+      "monthlyGrowth": "10.7",
+      "largeBizRetailAmount": "12253.3",
+      "largeBizMonthlyGrowth": "9.2"
+    },
+    {
+      "time": "2017年5月",
+      "retailAmount": "29459.2",
+      "monthlyGrowth": "10.7",
+      "largeBizRetailAmount": "12937.1",
+      "largeBizMonthlyGrowth": "9.2"
+    },
+    {
+      "time": "2017年6月",
+      "retailAmount": "29807.6",
+      "monthlyGrowth": "11.0",
+      "largeBizRetailAmount": "14222.0",
+      "largeBizMonthlyGrowth": "10.2"
+    },
+    {
+      "time": "2017年7月",
+      "retailAmount": "29609.8",
+      "monthlyGrowth": "10.4",
+      "largeBizRetailAmount": "12812.6",
+      "largeBizMonthlyGrowth": "8.6"
+    },
+    {
+      "time": "2017年8月",
+      "retailAmount": "30329.7",
+      "monthlyGrowth": "10.1",
+      "largeBizRetailAmount": "13229.4",
+      "largeBizMonthlyGrowth": "7.5"
+    },
+    {
+      "time": "2017年9月",
+      "retailAmount": "30870.3",
+      "monthlyGrowth": "10.3",
+      "largeBizRetailAmount": "14629.7",
+      "largeBizMonthlyGrowth": "7.8"
+    },
+    {
+      "time": "2017年10月",
+      "retailAmount": "34240.9",
+      "monthlyGrowth": "10.0",
+      "largeBizRetailAmount": "14373.8",
+      "largeBizMonthlyGrowth": "7.2"
+    },
+    {
+      "time": "2017年11月",
+      "retailAmount": "34108.2",
+      "monthlyGrowth": "10.2",
+      "largeBizRetailAmount": "15778.7",
+      "largeBizMonthlyGrowth": "7.8"
+    },
+    {
+      "time": "2017年12月",
+      "retailAmount": "34734.1",
+      "monthlyGrowth": "9.4",
+      "largeBizRetailAmount": "17409.4",
+      "largeBizMonthlyGrowth": "6.7"
+    },
+    {
+      "time": "2018年1-2月",
+      "retailAmount": "61081.8",
+      "monthlyGrowth": "9.7",
+      "largeBizRetailAmount": "23242.0",
+      "largeBizMonthlyGrowth": "8.3"
+    },
+    {
+      "time": "2018年3月",
+      "retailAmount": "29193.6",
+      "monthlyGrowth": "10.1",
+      "largeBizRetailAmount": "11831.8",
+      "largeBizMonthlyGrowth": "9.0"
+    },
+    {
+      "time": "2018年4月",
+      "retailAmount": "28541.9",
+      "monthlyGrowth": "9.4",
+      "largeBizRetailAmount": "11240.6",
+      "largeBizMonthlyGrowth": "7.8"
+    },
+    {
+      "time": "2018年5月",
+      "retailAmount": "30359.1",
+      "monthlyGrowth": "8.5",
+      "largeBizRetailAmount": "11476.7",
+      "largeBizMonthlyGrowth": "5.5"
+    }
+  ]
+}
